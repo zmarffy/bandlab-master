79982d27f7c0c0767b67de5c5e892fbdad2e34f1
Zeke Marffy <zmarffy@me.com>
2020-11-06 05:18:20 +0000

    pip refactor

---
+++ bandlab_master/__main__.py
@@ -0,0 +1,85 @@
+#! /usr/bin/env python3
+
+import os
+import sys
+from time import sleep
+
+import selenium
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_extended import Chrome
+from webdriver_extended.chrome.webelement import WebElement
+from zmtools import run_with_loading
+
+with open(os.path.join(os.path.expanduser("~"), ".bandlab", ".creds")) as f:
+    username, password = f.read().strip().split()
+
+
+def wait_until_master_ready(driver, username):
+    attempt_num = 0
+    while True:
+        attempt_num += 1
+        try:
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+                (By.CSS_SELECTOR, ".button-social[ng-controller='RevisionDownloadCtrl']"))).click()
+            break
+        except selenium.common.exceptions.TimeoutException as e:
+            if attempt_num == 30:
+                raise e
+            else:
+                if driver.current_url.startswith(f"https://www.bandlab.com/{username}"):
+                    driver.get(driver.current_url)
+
+
+# TODO: Delete master from BandLab once done. Dynamically get wait times based on file size.
+
+def main():
+
+    if len(sys.argv) < 2:
+        raise ValueError("No file supplied")
+
+    if not os.path.isfile(sys.argv[1]):
+        raise FileNotFoundError(f"File {sys.argv[1]} does not exist")
+
+    options = Options()
+    options.headless = True
+    options.add_argument("--window-size=1920,1080")
+
+    driver = None
+
+    try:
+        driver = Chrome(options=options)
+        driver.get("https://bandlab.com/login")
+        log_in_button = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "button[type='submit']")))
+        driver.find_element_by_css_selector(
+            "input[name='username']").send_keys(username)
+        driver.find_element_by_css_selector(
+            "input[name='password']").send_keys(password)
+        log_in_button.click()
+        # Possibly change this sleep to a more specific wait
+        sleep(4)
+        driver.get("https://www.bandlab.com/upload")
+        file_upload_form = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "input[type='file']")))
+        file_upload_form.send_keys(os.path.abspath(sys.argv[1]))
+        WebDriverWait(driver, 20).until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, "button[type='submit'].scs"))).click()
+        run_with_loading(wait_until_master_ready,
+                         phrase="Mastering", args=[driver, username])
+        WebDriverWait(driver, 3).until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, "label[for='versionMastered']"))).click()
+        driver.find_elements_by_css_selector("div.label")[-1].click()
+        run_with_loading(WebElement.click_to_download, phrase="Downloading master", args=[driver.find_element_by_css_selector(
+            ".form-field-submit button[type='submit']")], kwargs={"max_download_started_check_num": 40})
+    finally:
+        if driver is not None:
+            driver.save_screenshot("a.png")
+            driver.quit()
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ setup.py
@@ -0,0 +1,30 @@
+import re
+from os.path import join as join_path
+
+import setuptools
+
+with open(join_path("bandlab_master", "__init__.py"), encoding="utf8") as f:
+    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
+
+setuptools.setup(
+    name='bandlab-master',
+    version=version,
+    author='Zeke Marffy',
+    author_email='zmarffy@yahoo.com',
+    packages=setuptools.find_packages(),
+    url='https://github.com/zmarffy/bandlab-master',
+    license='MIT',
+    description='Master a song with BandLab',
+    python_requires='>=3.6',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'selenium',
+        'zmtools'
+    ],
+    entry_points={
+        'console_scripts': [
+            'bandlab-master = bandlab_master.__main__:main',
+        ],
+    },
+)
