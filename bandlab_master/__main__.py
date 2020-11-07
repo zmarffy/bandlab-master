d89a898a4a7aa52f51de3ec81de24bc93053c19b
Zeke Marffy <zmarffy@me.com>
2020-11-07 04:52:33 +0000

    final touches for pip refactor

--- bandlab_master/__main__.py
+++ bandlab_master/__main__.py
@@ -18,6 +18,25 @@ with open(os.path.join(os.path.expanduser("~"), ".bandlab", ".creds")) as f:
     username, password = f.read().strip().split()
 
 
+def login_and_upload(driver, username, password, file_to_master):
+    driver.get("https://bandlab.com/login")
+    log_in_button = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "button[type='submit']")))
+    driver.find_element_by_css_selector(
+        "input[name='username']").send_keys(username)
+    driver.find_element_by_css_selector(
+        "input[name='password']").send_keys(password)
+    log_in_button.click()
+    # Possibly change this sleep to a more specific wait
+    sleep(4)
+    driver.get("https://www.bandlab.com/upload")
+    file_upload_form = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "input[type='file']")))
+    file_upload_form.send_keys(os.path.abspath(file_to_master))
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located(
+        (By.CSS_SELECTOR, "button[type='submit'].scs"))).click()
+
+
 def wait_until_master_ready(driver, username):
     attempt_num = 0
     while True:
@@ -34,7 +53,8 @@ def wait_until_master_ready(driver, username):
                     driver.get(driver.current_url)
 
 
-# TODO: Delete master from BandLab once done. Dynamically get wait times based on file size.
+# TODO: Dynamically get wait times based on file size.
+
 
 def main():
 
@@ -54,22 +74,8 @@ def main():
 
     try:
         driver = Chrome(options=options)
-        driver.get("https://bandlab.com/login")
-        log_in_button = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "button[type='submit']")))
-        driver.find_element_by_css_selector(
-            "input[name='username']").send_keys(username)
-        driver.find_element_by_css_selector(
-            "input[name='password']").send_keys(password)
-        log_in_button.click()
-        # Possibly change this sleep to a more specific wait
-        sleep(4)
-        driver.get("https://www.bandlab.com/upload")
-        file_upload_form = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "input[type='file']")))
-        file_upload_form.send_keys(os.path.abspath(file_to_master))
-        WebDriverWait(driver, 20).until(EC.presence_of_element_located(
-            (By.CSS_SELECTOR, "button[type='submit'].scs"))).click()
+        run_with_loading(login_and_upload, phrase="Uploading track", args=[
+                         driver, username, password, file_to_master])
         run_with_loading(wait_until_master_ready,
                          phrase="Mastering", args=[driver, username])
         WebDriverWait(driver, 3).until(EC.presence_of_element_located(
@@ -77,9 +83,14 @@ def main():
         driver.find_elements_by_css_selector("div.label")[-1].click()
         run_with_loading(WebElement.click_to_download, phrase="Downloading master", args=[driver.find_element_by_css_selector(
             ".form-field-submit button[type='submit']")], kwargs={"max_download_started_check_num": 40})
+        driver.find_element_by_css_selector("button[type='button']").click()
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable(
+            (By.CSS_SELECTOR, "a span.text-cta"))).click()
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable(
+            (By.CSS_SELECTOR, "button.modal-confirm"))).click()
+        sleep(1)
     finally:
         if driver is not None:
-            driver.save_screenshot("a.png")
             driver.quit()
     return 0
 
